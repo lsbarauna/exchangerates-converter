c216ed783f6de436108286d578c09ecbe55f29bf
Luiz Barauna <luiz.moura@portoseguro.com.br>
2024-05-07 11:26:48 -0300

    refactor: remove Wildcard import

--- src/main/java/br/com/jaya/exchangerates/converter/controller/ExchangeRatesController.java
+++ src/main/java/br/com/jaya/exchangerates/converter/controller/ExchangeRatesController.java
@@ -20,7 +20,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 import java.util.List;
 
